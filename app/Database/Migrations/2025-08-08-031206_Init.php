891596c1e29682774047e661ba2ed45a0e9175bb
Wangseonwoo <dmb05007@naver.com>
2025-08-08 16:55:26 +0900

    first commit

---
+++ app/Database/Migrations/2025-08-08-031206_Init.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Init extends Migration
+{
+ public function up()
+    {
+        /**
+         * USERS TABLE
+         */
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'user_name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '50',
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+                'unique'     => true,
+            ],
+            'password' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'bio' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'login_count' => [
+                'type'       => 'INT',
+                'default'    => 0,
+            ],
+            'last_login_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
+        
+        /**
+         * TAGS TABLE
+         */
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'tag_name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('tags');
+
+        /**
+         * TASKS TABLE
+         */
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'user_id' => [
+                'type'     => 'INT',
+                'unsigned' => true,
+            ],
+            'tag_id' => [
+                'type'     => 'INT',
+                'unsigned' => true,
+            ],
+            'title' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'due_date' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'status' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'default'    => 'pending',
+            ],
+            'color' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'null'       => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('tag_id', 'tags', 'id', 'NO ACTION', 'CASCADE');
+        $this->forge->createTable('tasks');
+
+        /**
+         * FOLLOWS TABLE
+         */
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'follower_id' => [
+                'type'     => 'INT',
+                'unsigned' => true,
+            ],
+            'following_id' => [
+                'type'     => 'INT',
+                'unsigned' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('follower_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('following_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('follows');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('follows', true);
+        $this->forge->dropTable('tasks', true);
+        $this->forge->dropTable('tags', true);
+        $this->forge->dropTable('users', true);
+    }
+}
+
+
